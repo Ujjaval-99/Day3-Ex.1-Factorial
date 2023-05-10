4a0e5e15205e65519065d03993f393773a906467
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-10 11:19:09 +0530

    first commit

---
+++ FactorialNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter NO: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int factorial = CalculateFactorial(number);
+        Console.WriteLine($" The Factorial Of {number} is {factorial}");
+    }
+    public static int CalculateFactorial(int n)
+    {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+            result *= i;
+        }
+        return result;
+    }
+}
+
